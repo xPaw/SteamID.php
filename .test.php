542950efa5bb5bb4157398074b3e5e59e3239d9f
xPaw <github@xpaw.me>
2015-03-08 22:53:34 +0200

    ... I am an idiot

--- .test.php
+++ .test.php
@@ -255,7 +255,7 @@ class SteamIDFacts extends PHPUnit_Framework_TestCase
 		];
 	}
 	
-	public function vanityUrlProvider( )
+	public function invalidVanityUrlProvider( )
 	{
 		return
 		[
@@ -266,7 +266,7 @@ class SteamIDFacts extends PHPUnit_Framework_TestCase
 		];
 	}
 	
-	public function invalidVanityUrlProvider( )
+	public function vanityUrlProvider( )
 	{
 		return
 		[
